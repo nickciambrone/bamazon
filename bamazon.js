53196e0e4841a4e4d3af114b52fd03a9cd373f95
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2018-07-19 19:24:04 -0400

    first

---
+++ bamazon.js
@@ -0,0 +1,182 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+})
+function start() {
+    inquirer
+        .prompt({
+            name: "viewOrAdd",
+            type: "list",
+            choices: [
+                'View Products for Sale',
+                'View Low Inventory',
+                'Add to Inventory',
+                'Add New Product'
+            ]
+        }).then(function (answer) {
+            if (answer.viewOrAdd == 'Add New Product') {
+                addNewProduct()
+            }
+            if (answer.viewOrAdd == 'Add to Inventory') {
+                addToInventory()
+            }
+            if (answer.viewOrAdd=='View Products for Sale'){
+                viewProducts()
+            }
+            if (answer.viewOrAdd=='View Low Inventory'){
+                viewLow()
+            }
+        })
+}
+function viewLow(){
+    connection.query("SELECT * FROM inventory", function(err,results){
+        for (var i=0; i<results.length;i++){
+            if(results[i].quantity<5){
+            console.log('--------------------------------------------------------------------------------------------------------')
+            console.log("Item id: "+results[i].item_id+" | "+"Item Name: "+results[i].item_name+" | "+"Price: "+results[i].price+" | "+"Quantity: "+results[i].quantity+"\n")
+            }
+        }
+        start()
+
+    })
+}
+var num =25;
+var n=num.toString();
+console.log(n.length)
+
+function viewProducts(){
+
+    connection.query("SELECT * FROM inventory", function(err,results){
+        var spaces=[];
+        var spacesEnd=[];
+       
+        for (var i=0; i<results.length;i++){
+            var amountOfSpaces=14-results[i].item_name.length
+            for(var x=0;x<amountOfSpaces;x++){
+                spaces.push(" ")
+            }
+            // var mike = results[i].quantity.toString()
+            // var amountAtEnd=5-mike.length
+            // for (var x=0;x<amountAtEnd;i++){
+            //     spacesEnd.push(" ")
+            // }
+            console.log('\n-------------------------------------------------------------------------------------------------------')
+            console.log("| Item id: "+results[i].item_id+" | "+"Item Name: "+results[i].item_name+spaces.join("")+"| "+"Price: "+results[i].price+" | "+"Quantity: "+results[i].quantity+" |")
+            spaces=[];
+            spacesEnd=[];
+
+        }
+        
+    })
+    start()
+}
+function addNewProduct() {
+
+    inquirer.prompt([
+        {
+            name: "productName",
+            message: "Enter the Product Name"
+        },
+        {
+            name: "productPrice",
+            message: "Enter the Product Price"
+        },
+        {
+            name: "productQuantity",
+            message: "Enter the quantity that you would like to add"
+        }
+    ]).then(function (answer) {
+        connection.query(
+            "INSERT INTO inventory SET ?",
+            {
+                item_name: answer.productName,
+                price: answer.productPrice,
+                quantity: answer.productQuantity
+            },
+            function (err) {
+                if (err) throw err;
+                console.log("You have added " + answer.productName + " to the inventory.")
+                
+            }
+        )
+        start()
+    })
+
+
+}
+var newQuan;
+function addToInventory() {
+    connection.query("SELECT item_name FROM inventory", function (err, results) {
+        if (err) throw err;
+        // console.log(results)
+        inquirer.prompt([
+            {
+                name: "productName",
+                type: "list",
+                message: "Enter the name of the product that you would like to add to add to",
+                choices: function () {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].item_name)
+                    }
+                    return choiceArray;
+                }
+            },
+            {
+                name: "productQuantity",
+                message: "Enter the quantity that you would like to add"
+            }
+        ]).then(function (answer) {
+            var chosenItem = answer.productName
+            var quan=answer.productQuantity
+            
+            
+            var chosenItemQuantity;
+            connection.query("SELECT quantity FROM inventory WHERE ?", [
+                {
+                  item_name: chosenItem
+                }],function (err, results) {
+                    chosenItemQuantity=results[0].quantity;
+                    console.log(chosenItemQuantity)
+                    console.log(quan)
+                    var newQuan=parseInt(quan)+parseInt(chosenItemQuantity)
+                    console.log(newQuan)
+                    connection.query("UPDATE inventory SET ? WHERE ?",    
+                    [{
+                            quantity: newQuan
+                        },
+                        {
+                            item_name: chosenItem
+                        }],
+                        function (err) {
+                            if (err) throw err;
+                            // console.log("Added to Inventory successfully!");
+        
+                            connection.end()
+                        })
+                })
+        
+            
+           
+        })
+
+    })
+
+}
---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+-- DROP DATABASE IF EXISTS bamazonDB;
+-- CREATE DATABASE bamazonDB;
+
+-- USE bamazonDB;
+
+-- CREATE TABLE inventory (
+--     item_id INT NOT NULL AUTO_INCREMENT,
+--     item_name VARCHAR 100 NOT NULL,
+--     price INT NOT NULL,
+--     quantity INT NOT NULL
+--     PRIMARY KEY (item_id)
+
+-- )
