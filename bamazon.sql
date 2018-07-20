6ee6a4d761ab3be37b08d9d64780bc1924b9a3e3
nickciambrone <35977929+nickciambrone@users.noreply.github.com>
2018-07-19 20:02:44 -0400

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,13 +1,12 @@
--- DROP DATABASE IF EXISTS bamazonDB;
--- CREATE DATABASE bamazonDB;
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
 
--- USE bamazonDB;
+USE bamazonDB;
 
--- CREATE TABLE inventory (
---     item_id INT NOT NULL AUTO_INCREMENT,
---     item_name VARCHAR 100 NOT NULL,
---     price INT NOT NULL,
---     quantity INT NOT NULL
---     PRIMARY KEY (item_id)
+CREATE TABLE inventory (
+item_id INT NOT NULL AUTO_INCREMENT,
+item_name VARCHAR 100 NOT NULL,
+price INT NOT NULL,
+quantity INT NOT NULL
+PRIMARY KEY (item_id)
 
--- )
